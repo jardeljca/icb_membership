c332b42388420b9fbc35921f6fc8fd33aeebbc25
artursousac <artursousac@hotmail.com>
2024-11-20 18:55:39 -0300

    desenvolvimento da integracao com a tela de login

--- frontend/src/js/loginLider.js
+++ frontend/src/js/loginLider.js
@@ -1,4 +1,32 @@
-function logar() {
+import axios from 'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js'
+
+export async function logar() {
+    const emailLogin = document.getElementById('emailLogin').value;
+    const senhaLogin = document.getElementById('senhaLogin').value;
+  
+    try {
+      const response = await axios.post('https://backend-icb-membership.vercel.app/login/', {
+        email: emailLogin,
+        senha: senhaLogin,
+      });
+  
+      if (response.status === 200) {
+        console.log('Login bem-sucedido:', response.data);
+        alert('Login realizado com sucesso!');
+      }
+    } catch (error) {
+      if (error.response) {
+        console.error('Erro no login:', error.response.data);
+        alert('Erro: ' + error.response.data.message);
+      } else {
+        console.error('Erro na requisição:', error.message);
+        alert('Erro na requisição: ' + error.message);
+      }
+    }
+  }
+
+  /*
+  function logar() {
 
     const emailLogin = document.getElementById('emailLogin').value;
     const senhaLogin = document.getElementById('senhaLogin').value;
@@ -17,3 +45,4 @@ function logar() {
     }
 
 }
+  */
