67cedc9eaf291f9e0e8daa584bb6f41c20b9188b
Jardel Queiroz <jardeljcadm@gmail.com>
2024-11-23 03:00:51 -0300

    Criação das páginas de gerenciamento de membros e igrejas para separar da pagina de gerenciamneto geral

--- frontend/src/js/pagGerenciamento.js
+++ frontend/src/js/pagGerenciamento.js
@@ -1,29 +1,3 @@
-/* Botões */
-const btnMembros = document.getElementById("btnMembros");
-const btnConsultas = document.getElementById("btnConsultas");
-/* Divs */
-const areaMembros = document.getElementById("areaMembros");
-const areaConsultas = document.getElementById("areaConsultas");
-const logoIcb = document.getElementById("logoIcb");
-
-areaMembros.style.display = 'none';
-areaConsultas.style.display = 'none';
-
-btnMembros.addEventListener("click", () => {
-    areaMembros.style.display = 'flex';
-    if (areaConsultas.style.display === 'flex') {
-        areaConsultas.style.display = 'none';
-    }
-    logoIcb.style.display = 'none';
-})
-
-btnConsultas.addEventListener("click", () => {
-    areaConsultas.style.display = 'flex';
-    if (areaMembros.style.display === 'flex') {
-        areaMembros.style.display = 'none';
-    }
-})
-
 /* Funções que controlarão login e logout */
 
 function verificarSessao() {
@@ -49,40 +23,6 @@ function logout() {
     alert("Você foi desconectado!");
     window.location.href = "loginLider.html";
 }
-
-
-/* Adicionar membro cadastrado à tabela */
-/* import { membrosArray } from './cadastroMembro.js'; */
-/* 
-function listaTabela() {
-    let tbody = document.getElementById("tabelaMembros");
-
-    for (let i = 0; i < membrosArray.length; i++) {
-        let novaLinha = tbody.insertRow();
-
-        let colunaNome = novaLinha.insertCell();
-        let colunaAcoes = novaLinha.insertCell();
-
-        colunaNome.innerText = membrosArray[i].nome;
-
-        colunaAcoes.classList.add("corpoAcoes");
-
-        let imgVer = document.createElement("img");
-        imgVer.src = "../img/ver.png";
-
-        let imgEditar = document.createElement("img");
-        imgEditar.src = "../img/editar.png";
-
-        let imgExcluir = document.createElement("img");
-        imgExcluir.src = "../img/excluir.png";
-
-        colunaAcoes.appendChild(imgVer);
-        colunaAcoes.appendChild(imgEditar);
-        colunaAcoes.appendChild(imgExcluir);
-
-
-    }
-} */
 
 async function gerarPaginacao() {
     try {
@@ -139,7 +79,7 @@ async function getMembrosPaginado(inicio, fim) {
             row.innerHTML = `
                 <td>${membro.nome}</td>
                 <td>${membro.unidade}</td>
-                <td>
+                <td class="colunaAcoes">
                 <a type='button' href='#' class='acaoNome' onclick="visualizarMembro('${membro.id}')">Ver</a>
                 <a type='button' href='#' class='acaoNome' onclick="editarMembro('${membro.id}')">Editar</a>
                 <a type='button' href='#' class='acaoNome' onclick="deletarMembro('${membro.id}')">Excluir</a>
