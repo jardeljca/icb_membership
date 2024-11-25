02adc2ce6dbbe13b8cbaaa50ceb7c3de1c8369a0
artursousac <artursousac@hotmail.com>
2024-11-24 22:42:03 -0300

    .

--- frontend/src/js/cadastroMembro.js
+++ frontend/src/js/cadastroMembro.js
@@ -1,3 +1,16 @@
+function verificarSessao() {
+    if (!sessionStorage.getItem("access_token")) {
+        window.location.href = "loginLider.html";
+    }
+}
+window.onload = verificarSessao()
+
+function logout() {
+    sessionStorage.removeItem("access_token")
+    alert("Você foi desconectado!");
+    window.location.href = "loginLider.html";
+}
+
 /* Função para mascara do telefone */
 function formatarTelefone(input) {
     // Remove todos os caracteres não numéricos
@@ -9,6 +22,8 @@ function formatarTelefone(input) {
     // Atualiza o valor do campo de entrada com o telefone formatado
     input.value = telefone;
 }
+
+window.onload = gerarListaIgrejas()
 
 /* Função com API para buscar endereço de acordo com CEP */
 function buscaCep(cep) {
@@ -36,7 +51,7 @@ function buscaCep(cep) {
                 alert("Erro ao buscar o CEP. Tente novamente mais tarde.");
             });
     }
-    else{
+    else {
         alert("O cep precisa ter 8 digitos")
     }
 }
@@ -44,11 +59,11 @@ function buscaCep(cep) {
 /* Funções para mostrar ou esconder campo input data de batismo de acordo com a seleção da caixa */
 
 function mostrarData() {
-    const campoData = document.getElementById("campoData");campoData.style.display = "block";
+    const campoData = document.getElementById("campoData"); campoData.style.display = "block";
 }
 
 function esconderData() {
-    const campoData = document.getElementById("campoData");campoData.style.display = "none";
+    const campoData = document.getElementById("campoData"); campoData.style.display = "none";
     const dataBatismo = document.getElementById("dataBatismo");
     dataBatismo.value = "";
 }
@@ -67,7 +82,79 @@ function esconderAreaTexto() {
     tipoCargo.value = "";
 }
 
+async function gerarListaIgrejas() {
+    try {
+        verificarSessao()
+        const token = sessionStorage.getItem("access_token");
+        const formattedToken = token ? token.replace(/^"+|"+$/g, '') : null;
+        const response = await axios.get('https://backend-icb-membership.vercel.app/unidades', {
+            headers: {
+                'Authorization': `Bearer ${formattedToken}`,
+            },
+        })
+
+        const selectElement = document.getElementById("igreja");
+        selectElement.innerHTML = '<option value="" disabled selected>Selecione uma igreja</option>';
+
+        response.data.data.forEach((unidade) => {
+            const option = document.createElement("option");
+            option.value = unidade.id;
+            option.textContent = unidade.nome;
+            selectElement.appendChild(option);
+        });
+    } catch(error){
+        console.error("Erro ao carregar igrejas: ", error)
+    }
+}
+
+async function Cadastrar() {
+
+    try {
+        let nome = document.getElementById("nome").value;
+        let email = document.getElementById("email").value;
+        let dataNascimento = document.getElementById("dataNascimento").value;
+        let estadoCivil = document.getElementById("estadoCivil").value;
+        let unidade_id = document.getElementById("igreja").value;
+        let contato = document.getElementById("contato").value;
+        let profissao = document.getElementById("profissao").value;
+        let cep = document.getElementById("cep").value;
+        let endereco = document.getElementById("logradouro").value;
+        let numero = document.getElementById("numero").value;
+        let bairro = document.getElementById("bairro").value;
+        let cidade = document.getElementById("cidade").value;
+        let estado = document.getElementById("estado").value;
+        let batismoRadio = document.querySelector('input[name="batismo"]:checked');
+        let batismo = batismoRadio.value
+        let sexoRadio = document.querySelector('input[name="sexo"]:checked');
+        let sexo = sexoRadio.value
+        let posicao = "Membro"
+
+        const token = sessionStorage.getItem("access_token");
+        const formattedToken = token ? token.replace(/^"+|"+$/g, '') : null;
+        const response = await axios.post('https://backend-icb-membership.vercel.app/membro/', {
+            headers: {
+                'Authorization': `Bearer ${formattedToken}`,
+            },
+        },
+            { nome, email, dataNascimento, sexo, unidade_id, posicao, cep, estado, cidade, numero, endereco, bairro, estadoCivil, contato, profissao, batismo });
+
+        if (response.data.message == "Membro criado com sucesso") {
+            alert("Membro cadastrado com sucesso!!!")
+            window.location.replace('pagGerenciamento.html');
+        }
+    } catch (error) {
+        console.error(error.response.data);
+        if (error.response.data.detail == "Já existe um membro com este email") {
+            alert('Email já cadastrado')
+        } else {
+            alert('Erro no cadastro do membro')
+        }
+    }
+}
+
+
 /* Função para salvar novo membro */
+/*
 class Membro {
     constructor(nome, dataNascimento, estadoCivil, contato, escolaridade, instagram, endereco, dadosEclesiasticos) {
         this.nome = nome;
@@ -80,12 +167,11 @@ class Membro {
         this.dadosEclesiasticos = dadosEclesiasticos;
     }
 }
-
 const membrosArray = [];
 
 function salvarMembro(event) {
     event.preventDefault();
-    
+
     // Captura os valores do formulário através dos IDs
     const nome = document.getElementById("nome").value;
     const dataNascimento = document.getElementById("dataNascimento").value;
@@ -110,7 +196,7 @@ function salvarMembro(event) {
     } : { batizado: false };
 
     const cargoRadio = document.querySelector('input[name="cargo"]:checked');
-    const cargo = cargoRadio && cargoRadio.value === "sim" ? 
+    const cargo = cargoRadio && cargoRadio.value === "sim" ?
         document.getElementById("tipoCargo").value : null;
 
     // Cria o objeto endereço e dados eclesiásticos
@@ -130,6 +216,6 @@ function salvarMembro(event) {
     esconderData();
     esconderAreaTexto();
     console.log(membrosArray);
-}
 
-/* export { membrosArray }; */
+}*/
+// export { membrosArray };
