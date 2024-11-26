bbef01131886c245051287edf97da45f0c138ea3
artursousac <artursousac@hotmail.com>
2024-11-25 23:20:30 -0300

    .

--- frontend/src/js/loginLider.js
+++ frontend/src/js/loginLider.js
@@ -17,38 +17,27 @@ async function logar() {
   
       if (response.status == 200) {
         alert("Login realizado com sucesso")
+        esconderCarregando()
         sessionStorage.setItem('access_token', JSON.stringify(response.data.access_token));
         window.location.replace('pagGerenciamento.html');
       }
     } catch (error) {
         if (error.response && error.response.data) {
             alert(error.response.data.detail);
+            esconderCarregando()
         } else {
             alert("Erro ao conectar com o servidor.");
+            esconderCarregando()
         }
         document.getElementById('emailLogin').value = ""
         document.getElementById('senhaLogin').value = ""
     }
   }
 
-  /*
-  function logar() {
-
-    const emailLogin = document.getElementById('emailLogin').value;
-    const senhaLogin = document.getElementById('senhaLogin').value;
-
-    if (emailLogin == 'admin@admin.com'){
-        if(senhaLogin =='admin'){
-            localStorage.setItem("isLoggedIn", true);
-            location.href = "pagGerenciamento.html"
-        }
-        else{
-            alert("Senha incorreta!");
-        }
-    }
-    else{
-        alert("Usuário inexistente!");
-    }
-
+function mostrarCarregando() {
+    document.querySelector('.carregando').style.visibility = 'visible';
 }
-  */
+
+function esconderCarregando() {
+    document.querySelector('.carregando').style.visibility = 'hidden';
+}
