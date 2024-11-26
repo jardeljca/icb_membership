346f8c7191d057b5f9adb3aae132fa0632523d4e
artursousac <artursousac@hotmail.com>
2024-11-25 22:03:34 -0300

    .

--- frontend/src/js/cadastroMembroPublico.js
+++ frontend/src/js/cadastroMembroPublico.js
@@ -1,16 +1,3 @@
-function verificarSessao() {
-    if (!sessionStorage.getItem("access_token")) {
-        window.location.href = "loginLider.html";
-    }
-}
-window.onload = verificarSessao()
-
-function logout() {
-    sessionStorage.removeItem("access_token")
-    alert("Você foi desconectado!");
-    window.location.href = "loginLider.html";
-}
-
 /* Função para mascara do telefone */
 function formatarTelefone(input) {
     // Remove todos os caracteres não numéricos
